8104f59d5860949f7f9c2605d2989139647308af
Liz Miller <millere4@berea.edu>
2025-04-05 02:07:34 -0400

    sources updated from guis project

---
+++ Sources.py
@@ -0,0 +1,6 @@
+# tkinter- https://www.pythontutorial.net/tkinter/tkinter-optionmenu/?utm_source=chatgpt.com
+# tkinter boxes- https://www.youtube.com/watch?v=uRgjubz6sPo
+# tkinter- input boxes: https://python-course.eu/tkinter/entry-widgets-in-tkinter.php
+# tkinter-colormode: https://medium.com/tomtalkspython/tkinter-theming-creating-dark-mode-and-custom-color-schemes-f7f88dcbac6b
+# grid tips- https://stackoverflow.com/questions/76290490/python-tkinter-grid-doesnt-work-properly
+# dropdown menu- https://www.youtube.com/watch?v=R1IiIAp8wAY
